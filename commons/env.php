a7bc4f1458ef2dd66467e387daa6a3eeabc70360
Nguyen-Theanh <cpheo666@gmail.com>
2025-07-31 15:36:31 +0700

    DuAnMau

---
+++ commons/env.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('BASE_URL'       , 'http://localhost/duanmau/mvc-oop-basic/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'database');  // Tên database
+
+define('PATH_ROOT'    , __DIR__ . '/../');
